dae5cd4c9e524e87c9817aabdab02a30aa9abedd
benno <benno.loeffler@gmx.de>
2021-06-11 12:58:25 +0200

    wrap-hsts ssl-redirect wrap-forward-scheme

--- env/prod/clj/decision_konsent/prod_middleware.clj
+++ env/prod/clj/decision_konsent/prod_middleware.clj
@@ -3,9 +3,12 @@
     ;[ring.middleware.ssl :refer [wrap-reload]]
     ;[selmer.middleware :refer [wrap-error-page]]
     ;[prone.middleware :refer [wrap-exceptions]]
-    [ring.middleware.ssl :refer [wrap-hsts]]))
+    [ring.middleware.ssl :refer [wrap-hsts wrap-forwarded-scheme wrap-ssl-redirect]]))
 
 (defn wrap-prod [handler]
   (-> handler
-      ( wrap-hsts {:max-age 86400})))
+      wrap-forwarded-scheme
+      wrap-ssl-redirect
+      (wrap-hsts {:max-age 86400})))
 
+;(wrap-forwarded-scheme (wrap-ssl-redirect (wrap-hsts app)))
