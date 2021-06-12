50d0b74b3b6f1a321c9954b4c1d438e64e6f4389
benno <benno.loeffler@gmx.de>
2021-06-12 09:19:09 +0200

    wrap-hsts on day

--- env/prod/clj/decision_konsent/prod_middleware.clj
+++ env/prod/clj/decision_konsent/prod_middleware.clj
@@ -5,11 +5,21 @@
     ;[prone.middleware :refer [wrap-exceptions]]
     [ring.middleware.ssl :refer [wrap-hsts wrap-forwarded-scheme wrap-ssl-redirect]]))
 
+
+(defn wrap-print-request [handler when]
+ (fn [request]
+   (println "\n\n --------- " when "----------")
+   (puget.printer/cprint request)
+   request))
+
 (defn wrap-prod [handler]
       (-> handler
           ;wrap-hsts
-          wrap-forwarded-scheme
-          wrap-ssl-redirect))
+          (wrap-print-request "before ssl-redirect")
+          wrap-ssl-redirect
+          (wrap-print-request "before after ssl-redirect and before forwarded-scheme")
+          wrap-forwarded-scheme))
+
 ;(wrap-hsts {:max-age 86400})))
 
 ;(wrap-forwarded-scheme (wrap-ssl-redirect (wrap-hsts app)))
